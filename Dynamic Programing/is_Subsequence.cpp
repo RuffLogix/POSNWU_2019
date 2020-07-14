b83b6bad4e5734ac872e21eaa029e70f51272554
RuffLogix <teejuta.sriwaranon@gmail.com>
2020-07-14 20:25:40 +0700

    Add isSubsequence Problem.

---
+++ Dynamic Programing/is_Subsequence.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isSubsequence(string s, string t) {
+    int n_s = s.length()-1;
+    int n_t = t.length()-1;
+
+    for(int i=n_t ; i>=0 ; i--){
+        if(n_s==-1)return true;
+        if(t[i]==s[n_s]){
+            n_s--;
+        }
+    }
+    if(n_s==-1)return true;
+    return false;
+}
+
+
+int main(){
+    cout << isSubsequence("abc","ahbgdc");
+
+    return 0;
+}

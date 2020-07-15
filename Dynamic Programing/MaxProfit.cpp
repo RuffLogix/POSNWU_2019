fb929f0bdabd96353f622899b972e0e768989cb6
RuffLogix <teejuta.sriwaranon@gmail.com>
2020-07-15 19:09:11 +0700

    Added MaxProfit Problem.

---
+++ Dynamic Programing/MaxProfit.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+
+    int mx_profit = 0;
+    int min_prices = INT_MAX;
+    for(int i=0 ; i<prices.size() ; i++){
+        if(prices[i] < min_prices){
+            min_prices = prices[i];
+        }else if(mx_profit < prices[i] - min_prices){
+            mx_profit = prices[i] - min_prices;
+        }
+    }
+
+    return mx_profit;
+
+}
+
+int main(){
+    vector<int> a = {7,6,4,3,1};
+    cout << maxProfit(a);
+}
